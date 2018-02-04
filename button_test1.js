14453a60c1f1aaf482f08617c5725e3b99921418
eyw.dodh <36138404+yrur8i2@users.noreply.github.com>
2018-02-04 15:50:27 -0700

    t

--- button_test1.js
+++ button_test1.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
+
+    $( "#payment_success" ).show();
+    $( "#payment_pending" ).hide();
+    $( "#payment_remaining" ).hide();
+
     $("#yug_btn_success").click(function(){
-        alert("The paragraph was clicked.");
+        //alert("The paragraph was clicked.");
+        $( "#payment_success" ).show();
+        $( "#payment_pending" ).hide();
+        $( "#payment_remaining" ).hide();
+
     });
+
+    $("#yug_btn_pending").click(function(){
+        //alert("The paragraph was clicked.");
+        $( "#payment_success" ).hide();
+        $( "#payment_pending" ).show();
+        $( "#payment_remaining" ).hide();
+
+    });
+
+    $("#yug_btn_remaining").click(function(){
+        //alert("The paragraph was clicked.");
+        $( "#payment_success" ).hide();
+        $( "#payment_pending" ).hide();
+        $( "#payment_remaining" ).show();
+
+    });
+
+
+
 });
